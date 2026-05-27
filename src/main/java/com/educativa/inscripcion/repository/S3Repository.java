4e247b39dbf979dec0fff5357ac72a0c50db828e
dcruz-00 <di.cruzv@duocuc.cl>
2026-05-26 21:15:24 -0400

    agrega integración con AWS S3

---
+++ src/main/java/com/educativa/inscripcion/repository/S3Repository.java
@@ -0,0 +1,22 @@
+package com.educativa.inscripcion.repository;
+
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.educativa.inscripcion.model.Asset;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public interface S3Repository {
+
+    List<Asset> listObjectsInBucket(String bucket);
+
+    S3ObjectInputStream getObject(String bucketName, String fileName) throws IOException;
+
+    byte[] downloadFile(String bucketName, String fileName) throws IOException;
+
+    void moveObject(String bucketName, String fileKey, String destinationFileKey);
+
+    void deleteObject(String bucketName, String fileKey);
+
+    String uploadFile(String bucketName, String fileName, File fileObj);
+}
